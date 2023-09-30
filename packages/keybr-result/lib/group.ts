cec41c358be463cf27781a93c36a44c6b53aed92
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2023-09-30 09:10:45 +0200

    refactor: types and functions

--- packages/keybr-result/lib/group.ts
+++ packages/keybr-result/lib/group.ts
@@ -6,19 +6,38 @@ export type Group<T> = { key: T; results: Result[] };
 
 export type KeyOf<T> = (result: Result) => T;
 
+const layoutKey = (): KeyOf<Layout> => {
+  return ({ layout }) => layout;
+};
+
+const layoutFamilyKey = (): KeyOf<LayoutFamily> => {
+  return ({ layout }) => layout.family;
+};
+
+const dateKey = (): KeyOf<LocalDate> => {
+  let date = new LocalDate(0);
+  let next = new LocalDate(0);
+  return (result) => {
+    const { timeStamp } = result;
+    if (!(date.timeStamp <= timeStamp && timeStamp < next.timeStamp)) {
+      date = new LocalDate(timeStamp);
+      next = date.plusDays(1);
+    }
+    return date;
+  };
+};
+
 export class ResultGroups<T> implements Iterable<Group<T>> {
   static readonly byLayout = (results: Iterable<Result>) =>
-    new ResultGroups<Layout>().addAll(results, GroupBy.layout());
+    new ResultGroups<Layout>().addAll(results, layoutKey());
 
   static readonly byLayoutFamily = (results: Iterable<Result>) =>
-    new ResultGroups<LayoutFamily>().addAll(results, GroupBy.layoutFamily());
+    new ResultGroups<LayoutFamily>().addAll(results, layoutFamilyKey());
 
   static readonly byDate = (results: Iterable<Result>) =>
-    new ResultGroups<LocalDate>().addAll(results, GroupBy.date());
+    new ResultGroups<LocalDate>().addAll(results, dateKey());
 
   readonly #map = new Map<string, Group<T>>();
-
-  constructor() {}
 
   [Symbol.iterator](): IterableIterator<Group<T>> {
     return this.#map.values();
@@ -55,28 +74,4 @@ export class ResultGroups<T> implements Iterable<Group<T>> {
     }
     return this;
   }
-}
-
-export class GroupBy {
-  static readonly layout = (): KeyOf<Layout> => {
-    return (result: Result): Layout => result.layout;
-  };
-
-  static readonly layoutFamily = (): KeyOf<LayoutFamily> => {
-    return (result: Result): LayoutFamily => result.layout.family;
-  };
-
-  static readonly date = (): KeyOf<LocalDate> => {
-    let date = new LocalDate(0);
-    let next = new LocalDate(0);
-
-    return (result: Result): LocalDate => {
-      const { timeStamp } = result;
-      if (!(date.timeStamp <= timeStamp && timeStamp < next.timeStamp)) {
-        date = new LocalDate(timeStamp);
-        next = date.plusDays(1);
-      }
-      return date;
-    };
-  };
 }
