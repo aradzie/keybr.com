5fcfa19538add997678a32c6f2f81b4bfe113119
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2023-10-13 14:40:34 +0200

    fix: write a better explanation

--- packages/page-practice/lib/settings/lesson/DailyGoalSettings.tsx
+++ packages/page-practice/lib/settings/lesson/DailyGoalSettings.tsx
@@ -54,7 +54,7 @@ export function DailyGoalSettings(): ReactNode {
         <FormattedMessage
           id="settings.dailyGoalDescription"
           description="Description text."
-          defaultMessage="Set the time you want to spend on the exercises daily."
+          defaultMessage="Set the time you want to spend on the exercises daily. It is a simple reminder which does not limit you in any way. You can stop practicing whenever you want."
         />
       </Explainer>
     </FieldSet>
