ab381e86adc7cbda903a6ce19a3119cca6d45862
volking0 <volking0@gmail.com>
2023-10-16 06:48:32 -0400

    keyboard color consistency

--- packages/page-practice/lib/practice/Presenter.tsx
+++ packages/page-practice/lib/practice/Presenter.tsx
@@ -303,7 +303,7 @@ function NormalLayout({
           depressedKeys={depressedKeys}
           toggledKeys={toggledKeys}
           lastLesson={state.lastLesson}
-          showColors={settings.get(keyboardProps.colors) || !focus}
+          showColors={settings.get(keyboardProps.colors)}
         />
       </div>
       <Announcer state={state} />
