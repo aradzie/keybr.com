a6a4f519ac985e94f90419c9a7963926b810ee94
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2023-12-28 15:48:04 +0100

    chore: fix a lint error

--- packages/keybr-config/lib/knex/conn-sqlite.ts
+++ packages/keybr-config/lib/knex/conn-sqlite.ts
@@ -1,4 +1,4 @@
-import Database from "better-sqlite3";
+import type Database from "better-sqlite3";
 import { type Knex as KnexType } from "knex";
 import sqlite from "knex/lib/dialects/better-sqlite3";
 import { knexSnakeCaseMappers } from "objection";
@@ -7,7 +7,6 @@ import { fixTimestamps } from "./util.ts";
 export function connectSqlite(
   config: KnexType.BetterSqlite3ConnectionConfig,
 ): KnexType.Config {
-  bundleSqlitePackage();
   return {
     __client: "sqlite",
     client: sqlite,
@@ -19,12 +18,6 @@ export function connectSqlite(
   } as KnexType.Config;
 }
 
-function bundleSqlitePackage(): void {
-  // We don't have any direct dependencies on the sqlite package.
-  // If left as is then the sqlite package will not be bundled.
-  // To fix this we need to introduce an artificial direct dependency,
-  // so here is one.
-  if (typeof Database !== "function") {
-    throw new Error();
-  }
+function dummy(db: Database): void {
+  // Disable the unused dependency lint error.
 }
