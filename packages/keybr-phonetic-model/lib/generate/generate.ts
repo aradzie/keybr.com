2dd96ab3a9b05dcacb0a6db967de265c219260b3
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-01-09 18:17:17 +0100

    refactor: update the corpus of text

--- packages/keybr-phonetic-model/lib/generate/generate.ts
+++ packages/keybr-phonetic-model/lib/generate/generate.ts
@@ -6,7 +6,7 @@ import { gunzipSync, gzipSync } from "node:zlib";
 import { type CodePoint } from "@keybr/unicode";
 import { TransitionTableBuilder } from "./builder.ts";
 import { languages } from "./languages.ts";
-import { findWords, fromCsv, toCsv } from "./words.ts";
+import { findWords, fromCsv, sortByCount, toCsv } from "./words.ts";
 
 for (const { id, alphabet } of languages) {
   generateWords(id, alphabet);
@@ -24,8 +24,9 @@ function generateWords(id: string, alphabet: readonly CodePoint[]): void {
 function generateModel(id: string, alphabet: readonly CodePoint[]): void {
   const data = readCompressed(wordsPath(id));
   if (data != null) {
+    const words = fromCsv(data);
     const builder = new TransitionTableBuilder(4, [0x0020, ...alphabet]);
-    for (const [word, count] of fromCsv(data)) {
+    for (const [word, count] of words) {
       if (word.length >= 3) {
         for (let i = 0; i < count; i++) {
           builder.append(word);
@@ -34,6 +35,11 @@ function generateModel(id: string, alphabet: readonly CodePoint[]): void {
     }
     writeFileSync(dataPath(id), builder.build().compress());
     console.log(`Generated model ${id}`);
+
+    const wordsJson = sortByCount(words)
+      .slice(0, 3000)
+      .map(([word]) => word);
+    writeFileSync(wordsJsonPath(id), JSON.stringify(wordsJson, null, 2));
   }
 }
 
@@ -62,11 +68,15 @@ function writeCompressed(path: string, data: string): void {
 }
 
 function corpusPath(id: string): string {
-  return resolve(__dirname, "corpus", `${id}-corpus.txt`);
+  return resolve(__dirname, "corpus", `corpus-${id}.txt`);
 }
 
 function wordsPath(id: string): string {
-  return resolve(__dirname, "corpus", `${id}-words.csv`);
+  return resolve(__dirname, "corpus", `words-${id}.csv`);
+}
+
+function wordsJsonPath(id: string): string {
+  return resolve(__dirname, "corpus", `words-${id}.json`);
 }
 
 function dataPath(id: string): string {
--- packages/keybr-phonetic-model/lib/generate/languages.ts
+++ packages/keybr-phonetic-model/lib/generate/languages.ts
@@ -7,16 +7,16 @@ export type Language = {
 
 export const languages: readonly Language[] = [
   { id: "be", alphabet: "абвгдеёжзійклмнопрстуўфхцчшыьэюя" },
-  { id: "cs", alphabet: "aábcčdďeéěfghiíjklmnňoóprřsštťuúůvwxyýzž" },
+  { id: "cs", alphabet: "aábcčdďeéěfghiíjklmnňoóprřsštťuúůvxyýzž" },
   { id: "de", alphabet: "aäbcdefghijklmnoöpqrstuüvwxyzß" },
   { id: "en", alphabet: "abcdefghijklmnopqrstuvwxyz" },
   { id: "es", alphabet: "abcdefghijklmnñopqrstuvwxyz" },
-  { id: "fr", alphabet: "abcçdeéèfghijklmnopqrstuvwxyz" },
+  { id: "fr", alphabet: "abcçdeéèfghijlmnopqrstuvxyz" },
   { id: "it", alphabet: "abcdefghijklmnopqrstuvwxyz" },
   { id: "pl", alphabet: "aąbcćdeęfghijklłmnńoóprsśtuwyzźż" },
   { id: "pt", alphabet: "aáâãàbcçdeéêfghiíjklmnoóôõpqrstuúvwxyz" },
   { id: "ru", alphabet: "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" },
   { id: "sl", alphabet: "abcčdefghijklmnoprsštuvzž" },
-  { id: "sv", alphabet: "abcdefghijklmnoprstuvwxyzåäö" },
+  { id: "sv", alphabet: "abcdefghijklmnoprstuvwxyåäö" },
   { id: "uk", alphabet: "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя" },
 ].map(({ id, alphabet }) => ({ id, alphabet: [...toCodePoints(alphabet)] }));
