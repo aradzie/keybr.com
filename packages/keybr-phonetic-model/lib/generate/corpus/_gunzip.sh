654a7aa029a0ba2b661597c1b04b27156e2e6916
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-01-14 09:06:06 +0100

    chore: remove unused stuff

--- packages/keybr-phonetic-model/lib/generate/corpus/_gunzip.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for file in corpus-*.txt.gz; do
-  if [ -f "$file" ]; then
-    gunzip "$file"
-  fi
-done
--- packages/keybr-phonetic-model/lib/generate/corpus/_gzip.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for file in corpus-*.txt; do
-  if [ -f "$file" ]; then
-    gzip -9 "$file"
-  fi
-done
