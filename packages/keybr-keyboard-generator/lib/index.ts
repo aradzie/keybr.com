9f6397ddd5bf9cb6947e09d62bb088a2a5b822cb
Victor Tennekes <victor@ridedott.com>
2024-01-23 23:46:13 +0100

    fix/add-canary fix references

--- packages/keybr-keyboard-generator/lib/index.ts
+++ packages/keybr-keyboard-generator/lib/index.ts
@@ -4,10 +4,10 @@ import { type LayoutConfig } from "./layout.ts";
 import { LAYOUT_DE_BONE } from "./layout/layout_de_bone.ts";
 import { LAYOUT_DE_NEO_2 } from "./layout/layout_de_neo_2.ts";
 import { LAYOUT_EN_CUSTOM } from "./layout/layout_en_custom.ts";
+import { LAYOUT_EN_US_CANARY_MATRIX } from "./layout/layout_en_us_canary_matrix.ts";
 import { LAYOUT_EN_US_COLEMAK } from "./layout/layout_en_us_colemak.ts";
 import { LAYOUT_EN_US_COLEMAK_DH } from "./layout/layout_en_us_colemak_dh.ts";
 import { LAYOUT_EN_US_COLEMAK_DH_MATRIX } from "./layout/layout_en_us_colemak_dh_matrix.ts";
-import { LAYOUT_EN_US_CANARY_MATRIX } from "./layout/layout_en_us_canary_matrix.ts"
 import { LAYOUT_EN_US_WORKMAN } from "./layout/layout_en_us_workman.ts";
 import { LAYOUT_FR_BEPO } from "./layout/layout_fr_bepo.ts";
 import { LAYOUT_FR_ERGO_L } from "./layout/layout_fr_ergol.ts";
@@ -107,7 +107,7 @@ const files: readonly [input: string | LayoutConfig, output: string][] = [
   ],
   [
     LAYOUT_EN_US_CANARY_MATRIX,
-    "../keybr-keyboard/lib/data/layout/end_us_canary_matrix.ts"
+    "../keybr-keyboard/lib/data/layout/en_us_canary_matrix.ts",
   ],
   [LAYOUT_EN_US_WORKMAN, "../keybr-keyboard/lib/data/layout/en_us_workman.ts"],
   [LAYOUT_FR_BEPO, "../keybr-keyboard/lib/data/layout/fr_bepo.ts"],
