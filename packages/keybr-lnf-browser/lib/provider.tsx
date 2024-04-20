430e55233f95bc204a60fc253c16b501e9846ad5
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-04-20 12:50:28 +0200

    refactor: the ThemeProvider class to a function component

--- packages/keybr-lnf-browser/lib/provider.tsx
+++ packages/keybr-lnf-browser/lib/provider.tsx
@@ -7,123 +7,53 @@ import {
   ThemeContext,
   ThemePrefs,
 } from "@keybr/lnf";
-import { Component, type ReactNode } from "react";
-import { installPolyfills } from "./fullscreen-polyfill.ts";
+import { type ReactNode, useState } from "react";
+import { useFullscreen } from "./use-fullscreen.ts";
 
-const ON_FULLSCREEN_CHANGE = "fullscreenchange";
-const ON_FULLSCREEN_ERROR = "fullscreenerror";
+export function ThemeProvider({
+  children,
+}: {
+  readonly children: ReactNode;
+}): ReactNode {
+  const [fullscreenState, toggleFullscreen] = useFullscreen();
+  const [{ color, font }, setPrefs] = useState(() => readPrefs());
 
-type Props = {
-  readonly children?: ReactNode;
-};
-
-type State = {
-  readonly fullscreenState: boolean | null;
-  readonly color: ColorName;
-  readonly font: FontName;
-};
-
-export class ThemeProvider extends Component<Props, State> {
-  override state: State = {
-    fullscreenState: false,
-    color: "light",
-    font: "opensans",
+  const switchColor = (color: ColorName): void => {
+    const { id } = COLORS.findOption(color);
+    document.documentElement.setAttribute("data-color", id);
+    const prefs = new ThemePrefs({ color, font });
+    storePrefs(prefs);
+    setPrefs(prefs);
   };
 
-  override componentDidMount(): void {
-    installPolyfills();
-    this.setState(getInitialState());
-    document.addEventListener(
-      ON_FULLSCREEN_CHANGE, //
-      this.handleFullscreenChange,
-    );
-    document.addEventListener(
-      ON_FULLSCREEN_ERROR, //
-      this.handleFullscreenError,
-    );
-  }
-
-  override componentWillUnmount(): void {
-    document.removeEventListener(
-      ON_FULLSCREEN_CHANGE, //
-      this.handleFullscreenChange,
-    );
-    document.removeEventListener(
-      ON_FULLSCREEN_ERROR, //
-      this.handleFullscreenError,
-    );
-  }
-
-  override render(): ReactNode {
-    const { fullscreenState, color, font } = this.state;
-    return (
-      <ThemeContext.Provider
-        value={{
-          fullscreenState,
-          color,
-          font,
-          toggleFullscreen: this.toggleFullscreen,
-          switchColor: this.switchColor,
-          switchFont: this.switchFont,
-        }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
-
-  private readonly handleFullscreenChange = (): void => {
-    this.setState({ fullscreenState: document.fullscreenElement != null });
+  const switchFont = (font: FontName): void => {
+    const { id } = FONTS.findOption(font);
+    document.documentElement.setAttribute("data-font", id);
+    const prefs = new ThemePrefs({ color, font });
+    storePrefs(prefs);
+    setPrefs(prefs);
   };
 
-  private readonly handleFullscreenError = (): void => {
-    this.setState({ fullscreenState: null });
-  };
-
-  readonly toggleFullscreen = (): void => {
-    if (document.fullscreenElement == null) {
-      document.documentElement.requestFullscreen().catch(() => {
-        this.setState({ fullscreenState: null });
-      });
-    } else {
-      document.exitFullscreen().catch(() => {
-        this.setState({ fullscreenState: null });
-      });
-    }
-  };
-
-  readonly switchColor = (color: ColorName): void => {
-    this.setState({ color }, () => {
-      const { state } = this;
-      const { id } = COLORS.findOption(state.color);
-      document.documentElement.setAttribute("data-color", id);
-      storePrefs(new ThemePrefs(state));
-    });
-  };
-
-  readonly switchFont = (font: FontName): void => {
-    this.setState({ font }, () => {
-      const { state } = this;
-      const { id } = FONTS.findOption(state.font);
-      document.documentElement.setAttribute("data-font", id);
-      storePrefs(new ThemePrefs(state));
-    });
-  };
+  return (
+    <ThemeContext.Provider
+      value={{
+        fullscreenState,
+        color,
+        font,
+        toggleFullscreen,
+        switchColor,
+        switchFont,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
-function getInitialState(): State {
-  try {
-    let fullscreenState = null;
-    if (document.fullscreenEnabled) {
-      fullscreenState = document.fullscreenElement != null;
-    }
-    const { color, font } = ThemePrefs.deserialize(
-      Cookie.parse(document.cookie).get(ThemePrefs.cookieKey),
-    );
-    return { fullscreenState, color: color, font: font };
-  } catch {
-    return { fullscreenState: null, color: "light", font: "opensans" };
-  }
+function readPrefs(): ThemePrefs {
+  return ThemePrefs.deserialize(
+    Cookie.parse(document.cookie).get(ThemePrefs.cookieKey),
+  );
 }
 
 function storePrefs(prefs: ThemePrefs): void {
