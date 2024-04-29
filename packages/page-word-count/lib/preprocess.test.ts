5b798c97559ad9f1f2bb747f490e2c5421d70d63
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-04-29 16:13:07 +0200

    chore: remove the unused code

--- packages/page-word-count/lib/preprocess.test.ts
+++
@@ -1,9 +0,0 @@
-import test from "ava";
-import { preprocess } from "./preprocess.ts";
-
-test("flatten", (t) => {
-  preprocess("plain_text", "lorem ipsum");
-  preprocess("markdown", "lorem ipsum");
-
-  t.pass();
-});
