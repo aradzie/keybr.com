2e241481af73e08b233bfcd85e6f3b8902b15903
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-05-01 13:10:10 +0200

    feat: update the blacklists of words

--- packages/keybr-phonetic-model/lib/blacklist/blacklist.ts
+++ packages/keybr-phonetic-model/lib/blacklist/blacklist.ts
@@ -1,16 +1,20 @@
 import { Language } from "@keybr/keyboard";
-import blacklistBe from "./blacklist-be.json";
-import blacklistDe from "./blacklist-de.json";
-import blacklistEn from "./blacklist-en.json";
-import blacklistEs from "./blacklist-es.json";
-import blacklistFr from "./blacklist-fr.json";
-import blacklistIt from "./blacklist-it.json";
-import blacklistNl from "./blacklist-nl.json";
-import blacklistPl from "./blacklist-pl.json";
-import blacklistPt from "./blacklist-pt.json";
-import blacklistRu from "./blacklist-ru.json";
-import blacklistSv from "./blacklist-sv.json";
-import blacklistUk from "./blacklist-uk.json";
+import AR from "./blacklist-ar.json";
+import BE from "./blacklist-be.json";
+import DE from "./blacklist-de.json";
+import EN from "./blacklist-en.json";
+import ES from "./blacklist-es.json";
+import FA from "./blacklist-fa.json";
+import FR from "./blacklist-fr.json";
+import HE from "./blacklist-he.json";
+import IT from "./blacklist-it.json";
+import NL from "./blacklist-nl.json";
+import PL from "./blacklist-pl.json";
+import PT from "./blacklist-pt.json";
+import RU from "./blacklist-ru.json";
+import SV from "./blacklist-sv.json";
+import TR from "./blacklist-tr.json";
+import UK from "./blacklist-uk.json";
 import { unscrambleWord } from "./scramble.ts";
 
 const blacklistByLanguage = ((items: [Language, string[]][]) =>
@@ -20,18 +24,22 @@ const blacklistByLanguage = ((items: [Language, string[]][]) =>
       new Set(list.map(unscrambleWord)),
     ]),
   ))([
-  [Language.BE, blacklistBe],
-  [Language.DE, blacklistDe],
-  [Language.EN, blacklistEn],
-  [Language.ES, blacklistEs],
-  [Language.FR, blacklistFr],
-  [Language.IT, blacklistIt],
-  [Language.NL, blacklistNl],
-  [Language.PL, blacklistPl],
-  [Language.PT, blacklistPt],
-  [Language.RU, blacklistRu],
-  [Language.SV, blacklistSv],
-  [Language.UK, blacklistUk],
+  [Language.AR, AR],
+  [Language.BE, BE],
+  [Language.DE, DE],
+  [Language.EN, EN],
+  [Language.ES, ES],
+  [Language.FA, FA],
+  [Language.FR, FR],
+  [Language.HE, HE],
+  [Language.IT, IT],
+  [Language.NL, NL],
+  [Language.PL, PL],
+  [Language.PT, PT],
+  [Language.RU, RU],
+  [Language.SV, SV],
+  [Language.TR, TR],
+  [Language.UK, UK],
 ]);
 
 export type Blacklist = { readonly allow: (word: string) => boolean };
