cb319ed14b48496a57d67794c0163af185402d69
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-05-07 09:51:55 +0200

    chore: add a map of code point names

--- packages/keybr-generators/lib/layout/codepoints.ts
+++ packages/keybr-generators/lib/layout/codepoints.ts
@@ -1,33 +1,39 @@
-import { type CodePoint, getDiacritic } from "@keybr/unicode";
+import { type CodePoint } from "@keybr/unicode";
+
+const codePointNames = new Map([
+  [0x0020, "SPACE"],
+  [0x00a0, "NO-BREAK SPACE"],
+  [0x00ad, "SOFT HYPHEN"],
+  [0x0300, "COMBINING GRAVE ACCENT"],
+  [0x0301, "COMBINING ACUTE ACCENT"],
+  [0x0302, "COMBINING CIRCUMFLEX ACCENT"],
+  [0x0303, "COMBINING TILDE"],
+  [0x0304, "COMBINING MACRON"],
+  [0x0305, "COMBINING OVERLINE"],
+  [0x0306, "COMBINING BREVE"],
+  [0x0307, "COMBINING DOT ABOVE"],
+  [0x0308, "COMBINING DIAERESIS"],
+  [0x0309, "COMBINING HOOK ABOVE"],
+  [0x030a, "COMBINING RING ABOVE"],
+  [0x030b, "COMBINING DOUBLE ACUTE"],
+  [0x030c, "COMBINING CARON"],
+  [0x030d, "COMBINING VERTICAL LINE ABOVE"],
+  [0x030e, "COMBINING DOUBLE VERTICAL LINE ABOVE"],
+  [0x030f, "COMBINING DOUBLE GRAVE ACCENT"],
+  [0x0327, "COMBINING CEDILLA"],
+  [0x0328, "COMBINING OGONEK"],
+  [0x034f, "COMBINING GRAPHEME JOINER"],
+  [0x0384, "GREEK TONOS"],
+  [0x0385, "GREEK DIALYTIKA TONOS"],
+  [0x200c, "ZERO WIDTH NON-JOINER"],
+  [0x200d, "ZERO WIDTH JOINER"],
+  [0x200e, "LEFT-TO-RIGHT MARK"],
+  [0x200f, "RIGHT-TO-LEFT MARK"],
+  [0x202f, "NARROW NO-BREAK SPACE"],
+]);
 
 export function formatCodePointName(codePoint: CodePoint): string {
-  const diacritic = getDiacritic(codePoint);
-  if (diacritic != null) {
-    return `COMBINING ${diacritic.name}`;
-  } else {
-    switch (codePoint) {
-      case 0x0020:
-        return "SPACE";
-      case 0x00a0:
-        return "NO-BREAK SPACE";
-      case 0x00ad:
-        return "SOFT HYPHEN";
-      case 0x200c:
-        return "ZERO WIDTH NON-JOINER";
-      case 0x200d:
-        return "ZERO WIDTH JOINER";
-      case 0x200e:
-        return "LEFT-TO-RIGHT MARK";
-      case 0x200f:
-        return "RIGHT-TO-LEFT MARK";
-      case 0x202f:
-        return "NARROW NO-BREAK SPACE";
-      case 0x034f:
-        return "COMBINING GRAPHEME JOINER";
-      default:
-        return String.fromCodePoint(codePoint);
-    }
-  }
+  return codePointNames.get(codePoint) ?? String.fromCodePoint(codePoint);
 }
 
 export function formatCodePointValue(codePoint: CodePoint): string {
