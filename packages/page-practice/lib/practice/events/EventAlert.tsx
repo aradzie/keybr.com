93dd6dae038cd3f7ad2f191f82217978c1d9d436
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-05-15 11:56:02 +0200

    chore: translate more messages

--- packages/page-practice/lib/practice/events/EventAlert.tsx
+++ packages/page-practice/lib/practice/events/EventAlert.tsx
@@ -1,5 +1,6 @@
 import { Key } from "@keybr/lesson-ui";
 import { Award } from "@keybr/widget";
+import { FormattedMessage } from "react-intl";
 import { DailyGoalIcon, TrophyIcon } from "./icons.tsx";
 import { type Event } from "./types.ts";
 
@@ -8,20 +9,59 @@ export function EventAlert({ event }: { readonly event: Event }) {
     case "unlocked-letter":
       return (
         <Award icon={<Key lessonKey={event.lessonKey} size="announcement" />}>
-          New letter unlocked!
+          <FormattedMessage
+            id="event.unlockedLetter"
+            defaultMessage="New letter unlocked!"
+          />
         </Award>
       );
     case "unlocked-all-letters":
-      return <Award icon={<TrophyIcon />}>All letters unlocked!</Award>;
+      return (
+        <Award icon={<TrophyIcon />}>
+          <FormattedMessage
+            id="event.unlockedAllLetters"
+            defaultMessage="All letters unlocked!"
+          />
+        </Award>
+      );
     case "top-speed":
-      return <Award icon={<TrophyIcon />}>Top speed!</Award>;
+      return (
+        <Award icon={<TrophyIcon />}>
+          <FormattedMessage id="event.topSpeed" defaultMessage="Top speed!" />
+        </Award>
+      );
     case "top-score":
-      return <Award icon={<TrophyIcon />}>Top score!</Award>;
+      return (
+        <Award icon={<TrophyIcon />}>
+          <FormattedMessage id="event.topScore" defaultMessage="Top score!" />
+        </Award>
+      );
     case "accuracy-streak":
-      return <Award icon={<TrophyIcon />}>Accuracy streak!</Award>;
+      return (
+        <Award icon={<TrophyIcon />}>
+          <FormattedMessage
+            id="event.accuracyStreak"
+            defaultMessage="Accuracy streak!"
+          />
+        </Award>
+      );
     case "longest-accuracy-streak":
-      return <Award icon={<TrophyIcon />}>Longest accuracy streak!</Award>;
+      return (
+        <Award icon={<TrophyIcon />}>
+          <FormattedMessage
+            id="event.longestAccuracyStreak"
+            defaultMessage="Longest accuracy streak!"
+          />
+        </Award>
+      );
     case "daily-goal":
-      return <Award icon={<DailyGoalIcon />}>Daily goal reached!</Award>;
+      return (
+        <Award icon={<DailyGoalIcon />}>
+          <FormattedMessage
+            id="event.dailyGoal"
+            defaultMessage="Daily goal reached!"
+          />
+        </Award>
+      );
   }
 }
