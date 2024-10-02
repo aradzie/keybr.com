78868910b85aa329300ec36fb6991cf63f55d2d4
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-10-02 12:37:06 +0200

    fix: trophy icon sizes

--- packages/page-practice/lib/practice/state/event-icons.tsx
+++ packages/page-practice/lib/practice/state/event-icons.tsx
@@ -1,14 +1,26 @@
-import { Icon, styleCover } from "@keybr/widget";
+import { type ClassName } from "@keybr/widget";
 import { mdiAlarmCheck, mdiTrophy } from "@mdi/js";
 import { clsx } from "clsx";
 import * as styles from "./event-icons.module.less";
 
 export function TrophyIcon() {
-  return (
-    <Icon shape={mdiTrophy} className={clsx(styles.trophyIcon, styleCover)} />
-  );
+  return <Icon shape={mdiTrophy} className={styles.trophy} />;
 }
 
 export function DailyGoalIcon() {
-  return <Icon shape={mdiAlarmCheck} className={clsx(styleCover)} />;
+  return <Icon shape={mdiAlarmCheck} />;
+}
+
+function Icon({
+  shape,
+  className,
+}: {
+  readonly shape: string;
+  readonly className?: ClassName;
+}) {
+  return (
+    <svg className={clsx(styles.icon, className)} viewBox="0 0 24 24">
+      <path d={shape} />
+    </svg>
+  );
 }
