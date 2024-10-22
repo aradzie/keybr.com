10656424613d51378abdae26e27379118f3e5d2c
Wayne Van Son <=>
2024-10-23 10:01:20 +1100

    fix(settings): add info to repeat words

--- packages/page-practice/lib/settings/lesson/RepeatWordsProp.tsx
+++ packages/page-practice/lib/settings/lesson/RepeatWordsProp.tsx
@@ -1,14 +1,29 @@
+import { useIntlNumbers } from "@keybr/intl";
 import { lessonProps } from "@keybr/lesson";
 import { useSettings } from "@keybr/settings";
-import { Description, Explainer, Field, FieldList, Range } from "@keybr/widget";
+import {
+  Description,
+  Explainer,
+  Field,
+  FieldList,
+  Range,
+  Value,
+} from "@keybr/widget";
 import { type ReactNode } from "react";
 import { FormattedMessage } from "react-intl";
 
 export function RepeatWordsProp(): ReactNode {
+  const { formatPercents } = useIntlNumbers();
   const { settings, updateSettings } = useSettings();
   return (
     <>
       <FieldList>
+        <Field>
+          <FormattedMessage
+            id="settings.repeatWords.label"
+            defaultMessage="Add repeating words:"
+          />
+        </Field>
         <Field>
           <Range
             min={0}
@@ -18,6 +33,11 @@ export function RepeatWordsProp(): ReactNode {
             onChange={(value) => {
               updateSettings(settings.set(lessonProps.repeatWords, value));
             }}
+          />
+        </Field>
+        <Field>
+          <Value
+            value={formatPercents(settings.get(lessonProps.repeatWords))}
           />
         </Field>
       </FieldList>
