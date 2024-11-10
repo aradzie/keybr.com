83a8f76fd602a77235d480c27972b24095f646ef
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-11-10 11:11:46 +0100

    feat: update sound theme `typewriter1`

--- packages/keybr-textinput-sounds/lib/themes/typewriter1.ts
+++ packages/keybr-textinput-sounds/lib/themes/typewriter1.ts
@@ -3,10 +3,24 @@ import click2 from "../../assets/typewriter1-click2.mp3";
 import click3 from "../../assets/typewriter1-click3.mp3";
 import click4 from "../../assets/typewriter1-click4.mp3";
 import click5 from "../../assets/typewriter1-click5.mp3";
+import click6 from "../../assets/typewriter1-click6.mp3";
+import click7 from "../../assets/typewriter1-click7.mp3";
+import click8 from "../../assets/typewriter1-click8.mp3";
+import click9 from "../../assets/typewriter1-click9.mp3";
 import { type ThemeConfig } from "../sound.ts";
 import { common } from "./common.ts";
 
 export const typewriter1 = {
   ...common,
-  click: [click1, click2, click3, click4, click5],
+  click: [
+    click1,
+    click2,
+    click3,
+    click4,
+    click5,
+    click6,
+    click7,
+    click8,
+    click9,
+  ],
 } as const satisfies ThemeConfig;
