a32eaec91fbeed3ab57b1fb0c33d1dd5f2776d3e
Dikesh <dikesh.shrestha.010@gmail.com>
2025-02-14 16:00:35 +0000

    commented out other languages for potential ram efficiency

--- packages/keybr-content-words/lib/load.ts
+++ packages/keybr-content-words/lib/load.ts
@@ -11,41 +11,41 @@ export async function loadWordList(language: Language): Promise<WordList> {
         )
       ).default;
 
-    case Language.AR:
-      return (
-        await import(
-          /* webpackChunkName: "words-ar" */ "./data/words-ar.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.BE:
-      return (
-        await import(
-          /* webpackChunkName: "words-be" */ "./data/words-be.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.CS:
-      return (
-        await import(
-          /* webpackChunkName: "words-cs" */ "./data/words-cs.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.DE:
-      return (
-        await import(
-          /* webpackChunkName: "words-de" */ "./data/words-de.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.EL:
-      return (
-        await import(
-          /* webpackChunkName: "words-el" */ "./data/words-el.json",
-          { with: { type: "json" } }
-        )
-      ).default;
+    // case Language.AR:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-ar" */ "./data/words-ar.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.BE:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-be" */ "./data/words-be.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.CS:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-cs" */ "./data/words-cs.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.DE:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-de" */ "./data/words-de.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.EL:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-el" */ "./data/words-el.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
     case Language.EN:
       return (
         await import(
@@ -53,139 +53,139 @@ export async function loadWordList(language: Language): Promise<WordList> {
           { with: { type: "json" } }
         )
       ).default;
-    case Language.ES:
-      return (
-        await import(
-          /* webpackChunkName: "words-es" */ "./data/words-es.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.FA:
-      return (
-        await import(
-          /* webpackChunkName: "words-fa" */ "./data/words-fa.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.FR:
-      return (
-        await import(
-          /* webpackChunkName: "words-fr" */ "./data/words-fr.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.HE:
-      return (
-        await import(
-          /* webpackChunkName: "words-he" */ "./data/words-he.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.HR:
-      return (
-        await import(
-          /* webpackChunkName: "words-hr" */ "./data/words-hr.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.HU:
-      return (
-        await import(
-          /* webpackChunkName: "words-hu" */ "./data/words-hu.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.IT:
-      return (
-        await import(
-          /* webpackChunkName: "words-it" */ "./data/words-it.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.LT:
-      return (
-        await import(
-          /* webpackChunkName: "words-lt" */ "./data/words-lt.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.NB:
-      return (
-        await import(
-          /* webpackChunkName: "words-nb" */ "./data/words-nb.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.NL:
-      return (
-        await import(
-          /* webpackChunkName: "words-nl" */ "./data/words-nl.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.PL:
-      return (
-        await import(
-          /* webpackChunkName: "words-pl" */ "./data/words-pl.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.PT:
-      return (
-        await import(
-          /* webpackChunkName: "words-pt" */ "./data/words-pt.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.RO:
-      return (
-        await import(
-          /* webpackChunkName: "words-ro" */ "./data/words-ro.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.RU:
-      return (
-        await import(
-          /* webpackChunkName: "words-ru" */ "./data/words-ru.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.SL:
-      return (
-        await import(
-          /* webpackChunkName: "words-sl" */ "./data/words-sl.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.SV:
-      return (
-        await import(
-          /* webpackChunkName: "words-sv" */ "./data/words-sv.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.TH:
-      return (
-        await import(
-          /* webpackChunkName: "words-th" */ "./data/words-th.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.TR:
-      return (
-        await import(
-          /* webpackChunkName: "words-tr" */ "./data/words-tr.json",
-          { with: { type: "json" } }
-        )
-      ).default;
-    case Language.UK:
-      return (
-        await import(
-          /* webpackChunkName: "words-uk" */ "./data/words-uk.json",
-          { with: { type: "json" } }
-        )
-      ).default;
+    // case Language.ES:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-es" */ "./data/words-es.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.FA:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-fa" */ "./data/words-fa.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.FR:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-fr" */ "./data/words-fr.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.HE:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-he" */ "./data/words-he.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.HR:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-hr" */ "./data/words-hr.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.HU:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-hu" */ "./data/words-hu.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.IT:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-it" */ "./data/words-it.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.LT:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-lt" */ "./data/words-lt.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.NB:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-nb" */ "./data/words-nb.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.NL:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-nl" */ "./data/words-nl.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.PL:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-pl" */ "./data/words-pl.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.PT:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-pt" */ "./data/words-pt.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.RO:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-ro" */ "./data/words-ro.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.RU:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-ru" */ "./data/words-ru.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.SL:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-sl" */ "./data/words-sl.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.SV:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-sv" */ "./data/words-sv.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.TH:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-th" */ "./data/words-th.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.TR:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-tr" */ "./data/words-tr.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
+    // case Language.UK:
+    //   return (
+    //     await import(
+    //       /* webpackChunkName: "words-uk" */ "./data/words-uk.json",
+    //       { with: { type: "json" } }
+    //     )
+    //   ).default;
     default:
       throw new Error();
   }
--- packages/keybr-phonetic-model-loader/lib/assets.ts
+++ packages/keybr-phonetic-model-loader/lib/assets.ts
@@ -1,85 +1,85 @@
 import { Language } from "@keybr/keyboard";
-import AR from "@keybr/phonetic-model/assets/model-ar.data";
-import BE from "@keybr/phonetic-model/assets/model-be.data";
-import CS from "@keybr/phonetic-model/assets/model-cs.data";
-import DE from "@keybr/phonetic-model/assets/model-de.data";
-import EL from "@keybr/phonetic-model/assets/model-el.data";
+// import AR from "@keybr/phonetic-model/assets/model-ar.data";
+// import BE from "@keybr/phonetic-model/assets/model-be.data";
+// import CS from "@keybr/phonetic-model/assets/model-cs.data";
+// import DE from "@keybr/phonetic-model/assets/model-de.data";
+// import EL from "@keybr/phonetic-model/assets/model-el.data";
 import EN from "@keybr/phonetic-model/assets/model-en.data";
-import ES from "@keybr/phonetic-model/assets/model-es.data";
-import FA from "@keybr/phonetic-model/assets/model-fa.data";
-import FR from "@keybr/phonetic-model/assets/model-fr.data";
-import HE from "@keybr/phonetic-model/assets/model-he.data";
-import HR from "@keybr/phonetic-model/assets/model-hr.data";
-import HU from "@keybr/phonetic-model/assets/model-hu.data";
-import IT from "@keybr/phonetic-model/assets/model-it.data";
-import LT from "@keybr/phonetic-model/assets/model-lt.data";
-import NB from "@keybr/phonetic-model/assets/model-nb.data";
+// import ES from "@keybr/phonetic-model/assets/model-es.data";
+// import FA from "@keybr/phonetic-model/assets/model-fa.data";
+// import FR from "@keybr/phonetic-model/assets/model-fr.data";
+// import HE from "@keybr/phonetic-model/assets/model-he.data";
+// import HR from "@keybr/phonetic-model/assets/model-hr.data";
+// import HU from "@keybr/phonetic-model/assets/model-hu.data";
+// import IT from "@keybr/phonetic-model/assets/model-it.data";
+// import LT from "@keybr/phonetic-model/assets/model-lt.data";
+// import NB from "@keybr/phonetic-model/assets/model-nb.data";
 import NE from "@keybr/phonetic-model/assets/model-ne.data";
-import NL from "@keybr/phonetic-model/assets/model-nl.data";
-import PL from "@keybr/phonetic-model/assets/model-pl.data";
-import PT from "@keybr/phonetic-model/assets/model-pt.data";
-import RO from "@keybr/phonetic-model/assets/model-ro.data";
-import RU from "@keybr/phonetic-model/assets/model-ru.data";
-import SL from "@keybr/phonetic-model/assets/model-sl.data";
-import SV from "@keybr/phonetic-model/assets/model-sv.data";
-import TH from "@keybr/phonetic-model/assets/model-th.data";
-import TR from "@keybr/phonetic-model/assets/model-tr.data";
-import UK from "@keybr/phonetic-model/assets/model-uk.data";
+// import NL from "@keybr/phonetic-model/assets/model-nl.data";
+// import PL from "@keybr/phonetic-model/assets/model-pl.data";
+// import PT from "@keybr/phonetic-model/assets/model-pt.data";
+// import RO from "@keybr/phonetic-model/assets/model-ro.data";
+// import RU from "@keybr/phonetic-model/assets/model-ru.data";
+// import SL from "@keybr/phonetic-model/assets/model-sl.data";
+// import SV from "@keybr/phonetic-model/assets/model-sv.data";
+// import TH from "@keybr/phonetic-model/assets/model-th.data";
+// import TR from "@keybr/phonetic-model/assets/model-tr.data";
+// import UK from "@keybr/phonetic-model/assets/model-uk.data";
 
 export function modelAssetPath(language: Language): string {
   switch (language) {
     case Language.NE:
       return NE;
-    case Language.AR:
-      return AR;
-    case Language.BE:
-      return BE;
-    case Language.CS:
-      return CS;
-    case Language.DE:
-      return DE;
-    case Language.EL:
-      return EL;
+    // case Language.AR:
+    //   return AR;
+    // case Language.BE:
+    //   return BE;
+    // case Language.CS:
+    //   return CS;
+    // case Language.DE:
+    //   return DE;
+    // case Language.EL:
+    //   return EL;
     case Language.EN:
       return EN;
-    case Language.ES:
-      return ES;
-    case Language.FA:
-      return FA;
-    case Language.FR:
-      return FR;
-    case Language.HE:
-      return HE;
-    case Language.HR:
-      return HR;
-    case Language.HU:
-      return HU;
-    case Language.IT:
-      return IT;
-    case Language.LT:
-      return LT;
-    case Language.NB:
-      return NB;
-    case Language.NL:
-      return NL;
-    case Language.PL:
-      return PL;
-    case Language.PT:
-      return PT;
-    case Language.RO:
-      return RO;
-    case Language.RU:
-      return RU;
-    case Language.SL:
-      return SL;
-    case Language.SV:
-      return SV;
-    case Language.TH:
-      return TH;
-    case Language.TR:
-      return TR;
-    case Language.UK:
-      return UK;
+    // case Language.ES:
+    //   return ES;
+    // case Language.FA:
+    //   return FA;
+    // case Language.FR:
+    //   return FR;
+    // case Language.HE:
+    //   return HE;
+    // case Language.HR:
+    //   return HR;
+    // case Language.HU:
+    //   return HU;
+    // case Language.IT:
+    //   return IT;
+    // case Language.LT:
+    //   return LT;
+    // case Language.NB:
+    //   return NB;
+    // case Language.NL:
+    //   return NL;
+    // case Language.PL:
+    //   return PL;
+    // case Language.PT:
+    //   return PT;
+    // case Language.RO:
+    //   return RO;
+    // case Language.RU:
+    //   return RU;
+    // case Language.SL:
+    //   return SL;
+    // case Language.SV:
+    //   return SV;
+    // case Language.TH:
+    //   return TH;
+    // case Language.TR:
+    //   return TR;
+    // case Language.UK:
+    //   return UK;
     default:
       throw new Error();
   }
