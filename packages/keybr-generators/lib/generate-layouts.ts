ef83bd55b706141faff13739242f2c800e9dd0ac
Lucas Brandstaetter <lucas@brandstaetter.tech>
2025-03-02 12:28:42 +0100

    Add Focal layout

--- packages/keybr-generators/lib/generate-layouts.ts
+++ packages/keybr-generators/lib/generate-layouts.ts
@@ -52,6 +52,7 @@ for (const [id, layout] of [
   ["en_workman", importKeymap("layouts/en_workman.json")],
   ["en_workman_prog", importKeymap("layouts/en_workman_prog.json")],
   ["en-aptv3", importKeymap("layouts/en_aptv3.json")],
+  ["en_focal", importKeymap("layouts/en_focal.json")],
   ["es_es", importCldr("cldr-keyboards-43.0/keyboards/windows/es-t-k0-windows.xml")],
   ["es_mx", importCldr("cldr-keyboards-43.0/keyboards/windows/es-419-t-k0-windows.xml")],
   ["fa_ir", importCldr("cldr-keyboards-43.0/keyboards/windows/fa-t-k0-windows-extended.xml")],
