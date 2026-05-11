486aae5c6be02e3d9be03549038352d174c7ab8f
fgueben <fgueben@hotmail.com>
2026-05-11 20:49:54 +0200

    Update language.ts
    Added the letters "k" and "w" that were missing in the french language.
    
    abcçdeéèfghijlmnopqrstuvxyz -> is the previous alphabet (missing the letters "k" and  "w")
    
    abcçdeéèfghijklmnopqrstuvwxyz -> is the correct complete alphabet (by adding the letters "k" and "w")

--- packages/keybr-keyboard/lib/language.ts
+++ packages/keybr-keyboard/lib/language.ts
@@ -84,7 +84,7 @@ export class Language implements EnumItem {
     /* id= */ "fr",
     /* script= */ "latin",
     /* direction= */ "ltr",
-    /* alphabet= */ "abcçdeéèfghijlmnopqrstuvxyz",
+    /* alphabet= */ "abcçdeéèfghijklmnopqrstuvwxyz",
   );
   static readonly HE = new Language(
     /* id= */ "he",
